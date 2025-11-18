48ef3f01eb5060525c37173197ddb8f987b5d6a9
RidhoPK21 <142557165+RidhoPK21@users.noreply.github.com>
2025-11-18 20:39:02 +0700

    1

--- models/_index.ts
+++ models/_index.ts
@@ -2,7 +2,14 @@ import type { Model, ModelStatic } from "sequelize";
 
 // Import all models here
 import HakAksesModel from "./HakAksesModel";
+import TodoModel from "./TodoModel";
 
-const models: ModelStatic<Model>[] = [HakAksesModel];
+const models: ModelStatic<Model>[] = [
+  // Hak Akses
+  HakAksesModel,
+
+  // Todos
+  TodoModel,
+];
 
 export default models;
