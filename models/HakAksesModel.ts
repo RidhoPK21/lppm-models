a8309f3851ba4c1a7a66855e09ae18d1aa62d14c
RidhoPK21 <142557165+RidhoPK21@users.noreply.github.com>
2025-11-24 17:31:31 +0700

    hak akses

--- models/HakAksesModel.ts
+++ models/HakAksesModel.ts
@@ -2,18 +2,18 @@ import { DataTypes } from "sequelize";
 import db from "../utils/dbUtil";
 
 const TABLE_NAME = "m_hak_akses";
+
 const DataModel = db.define(
   TABLE_NAME,
   {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
+      primaryKey: true, // primaryKey hanya pada ID
     },
     user_id: {
       type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
+      allowNull: false, // Tidak perlu defaultValue dan jangan primaryKey
     },
     akses: {
       type: DataTypes.TEXT,
