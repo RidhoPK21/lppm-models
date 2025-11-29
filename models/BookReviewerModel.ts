5eda580f823688d2a09e8064806d397a64ee0d8f
RidhoPK21 <142557165+RidhoPK21@users.noreply.github.com>
2025-11-29 19:01:15 +0700

    push

--- models/BookReviewerModel.ts
+++ models/BookReviewerModel.ts
@@ -4,7 +4,7 @@ import sequelize from "../utils/dbUtil";
 interface BookReviewerAttributes {
   id: number;
   book_submission_id: number;
-  reviewer_id: string; // Dosen yang ditunjuk (Wajib)
+  user_id: string; // [UBAH] Diganti dari reviewer_id menjadi user_id
   review_note?: string;
   review_date?: Date;
   status: "PENDING" | "COMPLETED" | "DECLINED";
@@ -22,7 +22,7 @@ class BookReviewerModel
 {
   public id!: number;
   public book_submission_id!: number;
-  public reviewer_id!: string;
+  public user_id!: string; // [UBAH] Diganti dari reviewer_id menjadi user_id
   public review_note!: string;
   public review_date!: Date;
   public status!: "PENDING" | "COMPLETED" | "DECLINED";
@@ -35,7 +35,10 @@ BookReviewerModel.init(
   {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     book_submission_id: { type: DataTypes.INTEGER, allowNull: false },
-    reviewer_id: { type: DataTypes.UUID, allowNull: false },
+
+    // [UBAH] Nama kolom disesuaikan agar cocok dengan Laravel (where user_id = ...)
+    user_id: { type: DataTypes.UUID, allowNull: false },
+
     review_note: { type: DataTypes.TEXT, allowNull: true },
     review_date: { type: DataTypes.DATE, allowNull: true },
     status: {
